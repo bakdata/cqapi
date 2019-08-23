4b7791d16d5a8a43ca503b710a4ce66c6bf4877d
Frederic Schneider <frederic.schneider@bakdata.com>
2019-08-23 15:24:26 +0200

    add date_restriction support to util.add_selects..
    with the now supported CONCEPT_QUERY, AND, OR, NEGATION,
    DATE_RESTRICTION, and CONCEPT query node types the function covers all
    possible building blocks for queries that can be constructed using the
    frontend's query editor.

--- tests/util_test.py
+++ tests/util_test.py
@@ -160,6 +160,49 @@ query_without_concept = {
     }
 }
 
+query_with_daterange = {
+    "type": "CONCEPT_QUERY",
+    "root": {
+        "type": 'AND',
+        "children": [
+            {
+                "type": "OR",
+                "children": [
+                    {
+                        "excludeFromTimeAggregation": False,
+                        "ids": ["other.id"],
+                        "label": "3771",
+                        "tables": [{"id": "demo.table"}],
+                        "type": "CONCEPT"
+                    },
+                    {
+                        "excludeFromTimeAggregation": False,
+                        "ids": ["yet_another.id"],
+                        "label": "1617",
+                        "tables": [{"id": "demo.table"}],
+                        "type": "CONCEPT"
+                    }
+                ]
+            },
+            {
+                "type": "DATE_RESTRICTION",
+                "dateRange": {
+                    "min": "1992-02-18",
+                    "max": "2019-08-23"
+                },
+                "child": {
+                    "type": "CONCEPT",
+                    "ids": ["target_concept.id"],
+                    "label": "target concept",
+                    "selects": ["some_other_select"],
+                    "tables": [{"id": "some.table"}],
+                    "excludeFromTimeAggregation": False
+                }
+            }
+        ]
+    }
+}
+
 
 def test_add_select_to_concept_without_match():
     enriched_query = add_selects_to_concept(query_without_concept, target_concept_id, [select_id])
@@ -177,3 +220,7 @@ def test_add_selects_to_concept_with_match_with_preexisting():
     enriched_query = add_selects_to_concept(query_with_concept_and_preexisting_selects, target_concept_id, [select_id])
     assert select_id in enriched_query['root']['children'][1]['selects']
 
+
+def test_add_selects_to_concept_with_date_restriction():
+    enriched_query = add_selects_to_concept(query_with_daterange, target_concept_id, [select_id])
+    assert select_id in enriched_query['root']['children'][1]['child']['selects']
